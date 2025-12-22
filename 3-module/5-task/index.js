e924e953a6fd13674b3b23ecbdae5e3f759cd3fa
AntVoronin <voronin13011990@gmail.com>
2025-12-22 22:56:33 +0300

    3-module 5-task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,21 @@
+// const inputData = '1 и -5.8 или 10 хотя 34 + -5.3 и 73'
+
 function getMinMax(str) {
-  // ваш код...
+
+  const arrStr = str.split(' ').filter(el => typeof (+el) === 'number' && isFinite(+el) && !isNaN(+el))
+  const arrNum = arrStr.map(el => +el)
+  // console.log(arrNum)
+  //1)
+  // return {
+  //   min: Math.min(...arrNum),
+  //   max: Math.max(...arrNum)
+  // }
+  //2)
+  arrNum.sort((a, b) => a - b)
+  return {
+    min: arrNum[0],
+    max: arrNum[arrNum.length - 1]
+  }  
 }
+
+// console.log(getMinMax(inputData))
